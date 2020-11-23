0d9f8037a683a3de460b7bddabe46394f3eb180f
Rodgers Nyangweso <47724155+nyangweso-rodgers@users.noreply.github.com>
2020-11-23 22:51:08 +0300

    docstring code description

--- understanding_APIs/fastapiapp.py
+++ understanding_APIs/fastapiapp.py
@@ -1,4 +1,17 @@
+'''
+What is GET? — In a GET request, we usually try to retrieve data using query parameters that are embedded in the query string itself. 
+For example, in the OMDB API, we use the GET request to specify the movie id and access key as part of the query itself.
+
+What is PUT? — An alternative to the GET request is the PUT request, where we send parameters using a payload, as we will see in the second method. 
+The payload is not part of the query string, and thus PUT is more secure.
+'''
+#pip install fastapi
+#pip install uvicorn
+
+# 1. The GET Way:
+## A simple FastAPI method to writing a GET API for our titanic model use case is as follows:
 from fastapi import FastAPI
+import uvicorn
 
 app = FastAPI()
 @app.get("/predict")
