336a42612a8a83276c3f4f2f562c85d4c11323d3
Rodgers Nyangweso <47724155+nyangweso-rodgers@users.noreply.github.com>
2021-03-20 14:49:44 +0300

    initial commit to learning Dart programming language

---
+++ Dart_Programming_Language/dart_functions.dart
@@ -0,0 +1,8 @@
+void main() {   
+   var num = 12;   
+   if (num % 2 = = 0) {   
+      print("Number is Even.");   
+   } else {   
+      print("Number is Odd.");   
+   }   
+}  
---
+++ Dart_Programming_Language/hello.dart
@@ -0,0 +1,7 @@
+void main(){
+    String text1 = 'this is a single line string';
+    String text2 = '''this is a multiline
+	line string with a break''';
+print(text1);
+print(text2);
+}
