860ca570bd21106748a3d26f28d3c30ce95fe5ec
Rodgers Nyangweso <rodgerso65@gmail.com>
2022-08-08 13:26:18 +0300

    deleted java folder

--- Java/MyFirstJavaAppUsingPackages/src/com/rodgers/myfirstappusingpackages/Main.java
+++
@@ -1,8 +0,0 @@
-package com.rodgers.myfirstappusingpackages; // package declaration
-
-public class Main {
-
-    public static void main(String[] args) {
-	System.out.println("using packages in java");
-    }
-}
