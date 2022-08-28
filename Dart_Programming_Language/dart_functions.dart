ccc0da1e55790a47db37e5a966b8d43907dafb0f
Rodgers Nyangweso <rodgerso65@gmail.com>
2022-08-28 16:01:25 +0300

    -- dart folder

--- Dart_Programming_Language/dart_functions.dart
+++
@@ -1,8 +0,0 @@
-void main() {   
-   var num = 12;   
-   if (num % 2 = = 0) {   
-      print("Number is Even.");   
-   } else {   
-      print("Number is Odd.");   
-   }   
-}  
--- Dart_Programming_Language/hello.dart
+++
@@ -1,7 +0,0 @@
-void main(){
-    String text1 = 'this is a single line string';
-    String text2 = '''this is a multiline
-	line string with a break''';
-print(text1);
-print(text2);
-}
