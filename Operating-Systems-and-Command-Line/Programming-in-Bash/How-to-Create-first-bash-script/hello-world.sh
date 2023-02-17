454fb5a2a310b7f9bc9b2d11361c6e97d5fec68c
Rodgers Nyangweso <rodgerso65@gmail.com>
2023-02-17 15:47:41 +0300

    ++ reading a number using a Shell Script

--- Operating-Systems-and-Command-Line/Programming-in-Bash/How-to-Create-first-bash-script/hello-world.sh
+++ Operating-Systems-and-Command-Line/Programming-in-Bash/How-to-Create-first-bash-script/hello-world.sh
@@ -1,3 +1,5 @@
 
-# variable
+
+# Write a shell script to print hello world
 echo "Hello World, Bash"
+
---
+++ Operating-Systems-and-Command-Line/Programming-in-Bash/How-to-Create-first-bash-script/index.sh
@@ -0,0 +1,7 @@
+# Read a number in shell script
+# In Shell Script, we can read number using read keyword.
+echo "Enter a Number"
+
+read num
+
+echo "Number=$num"
