756d90a47c6fa8e3cfd42ea1cd2a88c15daea9e4
Rodgers Nyangweso <rodgerso65@gmail.com>
2023-02-18 15:21:42 +0300

    -deleted file

--- Algorithms_and_Data_Structures/Implementing_Hash_Table_in_Python.py
+++
@@ -1,5 +0,0 @@
-# Implementing Hash Table
-mydict = {'Apple' : 25 , 'Oranges' : 40 , 'Banana' :12 }
-print(mydict['Apple']) # Output: 25
-
-# Reference for the Implementation: https://carboncoffee.hashnode.dev/hash-table-in-python
